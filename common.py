0b11ff660e312ec04e7a4e8f56c8b5333dfb7cbf
kikaDev404 <vimalcmathew99@gmail.com>
2025-04-19 00:58:43 +0530

    added new date convertion code

--- common.py
+++ common.py
@@ -12,3 +12,14 @@ def load_df(filename : str, log):
         
 def config_log(log_name, log_file, log_level):
     return config.LoggerSetup(log_name = log_name, log_file=log_file, log_level=log_level)
+
+def convert_to_date(df : pd.DataFrame, date_list : list, log) -> pd.DataFrame:
+    try:
+        log.info(f"trying to convert the {date_list} to dates")
+        for date_col in date_list:
+            df[date_col] = pd.to_datetime(df[date_col])
+        log.info("done converting to date format")
+        return df
+    except Exception as ex:
+        log.error(ex)
+    
