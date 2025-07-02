f20c235f2c3545df8b894e1c7dc38337cf3650de
kikaDev404 <vimalcmathew99@gmail.com>
2025-07-03 00:53:50 +0530

    added logs for GT table creation

--- charts.py
+++ charts.py
@@ -21,26 +21,38 @@ def plot_bar_graph_stacked(df : pd.DataFrame, x_col : str, y_col : str, log, col
         log.error(f"An Error occured while plotting the graph : {ex}")
 
 def make_gt_table(df:pd.DataFrame, log, border_color : str = 'solid', table_align : str = 'center', col_header_color : str = 'black'):
-    gt_table = (
-        GT(df)
-        .tab_options(table_body_hlines_style=border_color, table_body_vlines_style=border_color, column_labels_vlines_style=border_color)
-        .cols_align(align=table_align, columns=None)
-        .tab_style(style=style.text(color=col_header_color, weight='bold'), locations=loc.column_labels())
-    )
+    log.info('Trying to make a GT table')
+    try:
+        gt_table = (
+            GT(df)
+            .tab_options(table_body_hlines_style=border_color, table_body_vlines_style=border_color, column_labels_vlines_style=border_color)
+            .cols_align(align=table_align, columns=None)
+            .tab_style(style=style.text(color=col_header_color, weight='bold'), locations=loc.column_labels())
+        )
+        log.info('Sucessfully made GT table')
+    except Exception as ex:
+        log.error(f'Error occured while making a GT table {ex}')
 
     return gt_table
 
 def add_gt_spanner(gt_obj : GT, lables_col_dict : dict, log,spanner_border_color : str = '#D3D3D3', spanner_text_color : str = 'black'):
-    for spanner_name in lables_col_dict:
-        gt_obj =  (
-            gt_obj.tab_spanner(label=spanner_name, columns=lables_col_dict[spanner_name])
-        )
-    
-    spanner_name_list = list(lables_col_dict.keys())
+    log.info('trying to add GT spanner')
+    try:
+        for spanner_name in lables_col_dict:
+            gt_obj =  (
+                gt_obj.tab_spanner(label=spanner_name, columns=lables_col_dict[spanner_name])
+            )
+        
+        spanner_name_list = list(lables_col_dict.keys())
 
-    gt_obj = (
-        gt_obj.tab_style(style.borders(sides='all', color=spanner_border_color, style = 'solid'), locations=loc.spanner_labels(ids=spanner_name_list))
-        .tab_style(style=style.text(color=spanner_text_color, weight='bold'), locations=loc.spanner_labels(ids=spanner_name_list))
-    )
+        gt_obj = (
+            gt_obj.tab_style(style.borders(sides='all', color=spanner_border_color, style = 'solid'), locations=loc.spanner_labels(ids=spanner_name_list))
+            .tab_style(style=style.text(color=spanner_text_color, weight='bold'), locations=loc.spanner_labels(ids=spanner_name_list))
+        )
+
+        log.info('Successfully added GT Spanner')
+    
+    except Exception as ex:
+        log.error(f'Error Occured while adding GT spanner: {ex}')
 
     return gt_obj
