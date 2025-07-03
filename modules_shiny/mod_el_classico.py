ecedb981a245e182eab3602d5fd2db526f5af60c
kikaDev404 <vimalcmathew99@gmail.com>
2025-07-03 12:36:59 +0530

    added overall page to el classico view

--- modules_shiny/mod_el_classico.py
+++ modules_shiny/mod_el_classico.py
@@ -25,7 +25,14 @@ def filter_el_classico(df, col_1, col_2):
 @module.ui
 def el_classico_ui() -> pd.DataFrame:
     return ui.card(
-        ui.card('Overall Classico Result', output_widget('overall_classico_result_plot')),
+        ui.card('Overall Classico Result',
+            ui.row(
+                ui.column(3, ui.value_box('Total played', ui.output_text('total_match_played'), showcase=output_widget('total_played_bargraph'),showcase_layout="bottom",style="height: 200px; width: 100%;")),
+                ui.column(3,ui.value_box('Won', ui.output_text('total_match_won'), showcase=output_widget('won_bargraph'),showcase_layout=("bottom"),style="height: 200px; width: 100%;")),
+                ui.column(3,ui.value_box('Draw', ui.output_text('total_match_drawed'), showcase=output_widget('draw_bargraph'),showcase_layout="bottom",style="height: 200px; width: 100%;")),
+                ui.column(3,ui.value_box('Lost', ui.output_text('total_match_lost'), showcase=output_widget('lost_bargraph'),showcase_layout="bottom",style="height: 200px; width: 100%;"))
+            ),
+        ),
         ui.row(
             ui.column(4, ui.card('EL Classico Season Data',ui.output_data_frame('season_summary_data_el_classico'))),
             ui.column(8, ui.card('El Classico Seasonal Result Graph', output_widget('el_classico_seasonal_plot'))),
@@ -36,6 +43,7 @@ def el_classico_ui() -> pd.DataFrame:
 @module.server
 def el_classico_server(input,output,session,match_played_place):
     season_data_el_classico = reactive.Value(None)
+    count_of_outcomes_el_classico = reactive.Value()
 
     @render.data_frame
     def season_summary_data_el_classico():
@@ -92,6 +100,113 @@ def el_classico_server(input,output,session,match_played_place):
         gt_table = add_gt_spanner(gt_table, {'Full Time Result' : ['Draw', 'Lost', 'Win']}, log = log)
         gt_table = gt_table.tab_options(table_width="50%")
         return ui.HTML(gt_table.as_raw_html())
+
+    @render_widget
+    def total_played_bargraph():
+        barca_data_filtered = apply_filter(barca_data, match_played_place(),log)
+        barca_data_filtered = filter_el_classico(barca_data_filtered, 'HomeTeam', 'AwayTeam')
+        # Group by Date and Match Result
+        temp = (
+            barca_data_filtered
+            .groupby(['Season'])
+            .size()
+            .reset_index(name='Number Of Games')
+            .sort_values('Season')
+        )
+        # Plot time series sparkline
+        fig = plot_bar_graph(temp,'Season','Number Of Games', log=log)
+        fig = conver_bar_plot_for_valuebox(fig,log)
+        return fig
+
+    @render_widget()
+    def won_bargraph():
+        barca_data_filtered = apply_filter(barca_data, match_played_place(), log)
+        barca_data_filtered = filter_el_classico(barca_data_filtered, 'HomeTeam', 'AwayTeam')
+        barca_data_filtered = barca_data_filtered.loc[barca_data_filtered['Match Result'] == 'Win']
+
+
+        # Group by Date and Match Result
+        temp = (
+            barca_data_filtered
+            .groupby(['Season'])
+            .size()
+            .reset_index(name='Number Of Games')
+            .sort_values('Season')
+        )
+        # Plot time series sparkline
+        fig = plot_bar_graph(temp,'Season','Number Of Games', log=log)
+        fig = conver_bar_plot_for_valuebox(fig,log)
+        return fig
+     
+    @render_widget()
+    def draw_bargraph():
+        barca_data_filtered = apply_filter(barca_data, match_played_place(), log)
+        barca_data_filtered = filter_el_classico(barca_data_filtered, 'HomeTeam', 'AwayTeam')
+        barca_data_filtered = barca_data_filtered.loc[barca_data_filtered['Match Result'] == 'Draw']
+
+
+        # Group by Date and Match Result
+        temp = (
+            barca_data_filtered
+            .groupby(['Season'])
+            .size()
+            .reset_index(name='Number Of Games')
+            .sort_values('Season')
+        )
+        # Plot time series sparkline
+        fig = plot_bar_graph(temp,'Season','Number Of Games', log=log)
+        fig = conver_bar_plot_for_valuebox(fig,log)
+        return fig
+     
+    @render_widget()
+    def lost_bargraph():
+        barca_data_filtered = apply_filter(barca_data, match_played_place(), log)
+        barca_data_filtered = filter_el_classico(barca_data_filtered, 'HomeTeam', 'AwayTeam')
+        barca_data_filtered = barca_data_filtered.loc[barca_data_filtered['Match Result'] == 'Lost'] 
+
+
+        # Group by Date and Match Result 
+        temp = (
+            barca_data_filtered
+            .groupby(['Season'])
+            .size()
+            .reset_index(name='Number Of Games')
+            .sort_values('Season')
+        )
+        # Plot time series sparkline
+        fig = plot_bar_graph(temp,'Season','Number Of Games', log=log)
+        fig = conver_bar_plot_for_valuebox(fig,log)
+        return fig
+    
+    @render.text
+    def total_match_played():
+         barca_data_filtered = apply_filter(barca_data, match_played_place(), log)
+         barca_data_filtered = filter_el_classico(barca_data_filtered, 'HomeTeam', 'AwayTeam')
+         count_outcome = barca_data_filtered.groupby('Match Result').size().reset_index(name='Count')
+         total_outcome_count = pd.DataFrame({'Match Result' : 'Total', 'Count' : [count_outcome['Count'].sum()]})
+
+         full_outcome_count = pd.concat([count_outcome, total_outcome_count], ignore_index=True)
+         count_of_outcomes_el_classico.set(full_outcome_count)
+
+         total_match_played_count = full_outcome_count.loc[full_outcome_count['Match Result'] == 'Total', 'Count'].values[0]
+         
+         return str(total_match_played_count)
+     
+    @render.text
+    def total_match_won():
+         count_outcomes = count_of_outcomes_el_classico()
+         return str(count_outcomes.loc[count_outcomes['Match Result'] == 'Win', 'Count'].values[0])
+     
+    @render.text
+    def total_match_drawed():
+         count_outcomes = count_of_outcomes_el_classico()
+         draw_df = count_outcomes.loc[count_outcomes['Match Result'] == 'Draw', 'Count']
+         return  str(draw_df.values[0]) if not draw_df.empty else "0"
+     
+    @render.text
+    def total_match_lost():
+         count_outcomes = count_of_outcomes_el_classico()
+         return str(count_outcomes.loc[count_outcomes['Match Result'] == 'Lost', 'Count'].values[0])
 
     
 
